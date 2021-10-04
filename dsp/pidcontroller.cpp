35e8b7d6d18f2dc97564543ed28273c7cd812051
DunkHimYo <57661649+DunkHimYo@users.noreply.github.com>
2021-10-05 04:07:34 +0900

    Update pidcontroller.cpp

--- dsp/pidcontroller.cpp
+++ dsp/pidcontroller.cpp
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the COPYING file.
- */
-
 #include <almath/dsp/pidcontroller.h>
 #include <iostream>
 #include <cmath>
