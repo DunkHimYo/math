84141395fe37c18921622bcccf8ef049dd0c910d
DunkHimYo <57661649+DunkHimYo@users.noreply.github.com>
2021-10-05 04:07:59 +0900

    Update digitalfilter.cpp

--- dsp/digitalfilter.cpp
+++ dsp/digitalfilter.cpp
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2012-2014 Aldebaran Robotics. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the COPYING file.
- */
-
 #include <almath/dsp/digitalfilter.h>
 
 namespace AL
